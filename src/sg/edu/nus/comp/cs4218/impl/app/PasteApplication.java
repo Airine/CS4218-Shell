71710fa9702d88206aae1eecd4c30295e74950f7
SUSTC11612405 <897815338@qq.com>
2020-02-18 20:48:02 +0800

    Finish implementation and test for Paste

--- src/sg/edu/nus/comp/cs4218/impl/app/PasteApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/PasteApplication.java
@@ -5,6 +5,7 @@ import sg.edu.nus.comp.cs4218.app.PasteInterface;
 import sg.edu.nus.comp.cs4218.exception.PasteException;
 
 import java.io.*;
+import java.util.ArrayList;
 
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.*;
 import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.CHAR_FILE_SEP;
@@ -25,13 +26,38 @@ public class PasteApplication implements PasteInterface {
     @Override
     public void run(String[] args, InputStream stdin, OutputStream stdout) throws PasteException {
         // Format: paste [FILE]...
-        if (args == null) {
+        if (args == null || args.length == 0) {
             throw new PasteException(ERR_NULL_ARGS);
         }
         if (stdout == null) {
-            throw new PasteException(ERR_NULL_STREAMS);
+            throw new PasteException(ERR_NO_OSTREAM);
         }
-        //TODO
+        String results;
+        boolean haveFile = false;
+        boolean haveStdin = false;
+        ArrayList<String> fileList = new ArrayList<>();
+        for (String arg : args) {
+            if ("-".equals(arg)) {
+                haveStdin = true;
+            } else {
+                haveFile = true;
+                fileList.add(arg);
+            }
+        }
+        String[] fileNames = new String[fileList.size()];
+        fileList.toArray(fileNames);
+        if (haveFile && haveStdin) {
+            results = mergeFileAndStdin(stdin, fileNames);
+        } else if (haveFile) {
+            results = mergeFile(fileNames);
+        } else {
+            results = mergeStdin(stdin);
+        }
+        try {
+            stdout.write(results.getBytes());
+        } catch (IOException e) {
+            throw new PasteException(ERR_IO_EXCEPTION);//NOPMD
+        }
     }
 
     /**
@@ -42,9 +68,20 @@ public class PasteApplication implements PasteInterface {
      * @throws Exception
      */
     @Override
-    public String mergeStdin(InputStream stdin) throws Exception {
-        //TODO
-        return null;
+    public String mergeStdin(InputStream stdin) throws PasteException {
+        if (stdin == null) {
+            throw new PasteException(ERR_NO_ISTREAM);
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stdin))) {
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(STRING_NEWLINE);
+            }
+            return output.toString();
+        } catch (IOException e) {
+            throw new PasteException(ERR_IO_EXCEPTION);//NOPMD
+        }
     }
 
     /**
@@ -55,7 +92,7 @@ public class PasteApplication implements PasteInterface {
      * @throws Exception
      */
     @Override
-    public String mergeFile(String... fileName) throws Exception {
+    public String mergeFile(String... fileName) throws PasteException {
         if (fileName == null || fileName.length == 0) {
             throw new PasteException(ERR_NULL_ARGS);
         }
@@ -71,8 +108,7 @@ public class PasteApplication implements PasteInterface {
                 if (file.isDirectory()) {
                     throw new PasteException(ERR_IS_DIR);
                 }
-                BufferedReader reader = new BufferedReader(new FileReader(path));
-                readers[i] = reader;
+                readers[i] = new BufferedReader(new FileReader(path));
             }
             String line;
             if (fileName.length == 1) { //echo back if only one file specified
@@ -82,28 +118,12 @@ public class PasteApplication implements PasteInterface {
                 readers[0].close();
                 return output.toString();
             }
-            int unfinished;
-            do {
-                unfinished = fileName.length;
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < fileName.length; i++) {
-                    line = readers[i].readLine();
-                    if (line == null) {
-                        builder.append(CHAR_TAB);
-                        unfinished--;
-                    } else {
-                        builder.append(line).append(CHAR_TAB);
-                    }
-                }
-                if (unfinished > 0) {
-                    output.append(builder.toString()).append(STRING_NEWLINE);
-                }
-            } while (unfinished > 0);
-            for (BufferedReader reader : readers) {
-                reader.close();
+            mergeAlgorithm(fileName.length, readers, output);
+            for (int i = 0; i < readers.length; i++) {
+                readers[i].close();
             }
         }catch (IOException e) {
-            throw new PasteException(ERR_IO_EXCEPTION);
+            throw new PasteException(ERR_IO_EXCEPTION);//NOPMD
         }
         return output.toString();
     }
@@ -116,9 +136,45 @@ public class PasteApplication implements PasteInterface {
      * @throws Exception
      */
     @Override
-    public String mergeFileAndStdin(InputStream stdin, String... fileName) throws Exception {
-        //TODO
-        return null;
+    public String mergeFileAndStdin(InputStream stdin, String... fileName) throws PasteException {
+        String mergeStdinResult = mergeStdin(stdin);
+        String mergeFileResult = mergeFile(fileName);
+        String[] stdinLines = mergeStdinResult.split(STRING_NEWLINE);
+        String[] fileLines = mergeFileResult.split(STRING_NEWLINE);
+        int numResultLines = Math.max(stdinLines.length, fileLines.length);
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < numResultLines; i++) {
+            if (i < stdinLines.length) {
+                output.append(stdinLines[i].trim());
+            }
+            output.append(CHAR_TAB);
+            if (i < fileLines.length) {
+                output.append(fileLines[i]);
+            }
+            output.append(STRING_NEWLINE);
+        }
+        return output.toString();
+    }
+
+    private void mergeAlgorithm(int fileNumber, BufferedReader[] readers, StringBuilder output) throws IOException {
+        String line;
+        int unfinished;
+        do {
+            unfinished = fileNumber;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < fileNumber; i++) {
+                line = readers[i].readLine();
+                if (line == null) {
+                    builder.append(CHAR_TAB);
+                    unfinished--;
+                } else {
+                    builder.append(line).append(CHAR_TAB);
+                }
+            }
+            if (unfinished > 0) {
+                output.append(builder.toString()).append(STRING_NEWLINE);
+            }
+        } while (unfinished > 0);
     }
 
 
