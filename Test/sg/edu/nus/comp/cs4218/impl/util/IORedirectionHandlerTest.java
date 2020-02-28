f5885899784c58ecae1c429c316a0c49f8bb0404
Mike <116010104@link.cuhk.edu.cn>
2020-02-28 23:13:53 +0800

    update IORedirectionHandlerTest

--- Test/sg/edu/nus/comp/cs4218/impl/util/IORedirectionHandlerTest.java
+++ Test/sg/edu/nus/comp/cs4218/impl/util/IORedirectionHandlerTest.java
@@ -1,13 +1,38 @@
 package sg.edu.nus.comp.cs4218.impl.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.EchoException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.app.TestFileUtils;
 
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NULL_ARGS;
 
 class IORedirectionHandlerTest {
+
+    private String testString = "Hello World";
+    @BeforeEach
+    void setUp() {
+        try {
+            TestFileUtils.createSomeFiles();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        TestFileUtils.rmCreatedFiles();
+    }
 
     @Test
     void testGetNoRedirArgsList(){
@@ -22,7 +47,7 @@ class IORedirectionHandlerTest {
 
     @Test
     void testGetInputStream(){
-        List<String> args = Arrays.asList("a", "b");
+        List<String> args = Arrays.asList("c", "d");
         ArgumentResolver resolver = new ArgumentResolver();
         IORedirectionHandler handler = new IORedirectionHandler(args, System.in, System.out, resolver);
         assertEquals(System.in, handler.getInputStream());
@@ -30,11 +55,45 @@ class IORedirectionHandlerTest {
 
     @Test
     void testGetOutputStream(){
-        List<String> args = Arrays.asList("a", "b");
+        List<String> args = Arrays.asList("e", "f");
         ArgumentResolver resolver = new ArgumentResolver();
         IORedirectionHandler handler = new IORedirectionHandler(args, System.in, System.out, resolver);
         assertEquals(System.out, handler.getOutputStream());
     }
 
+    @Test
+    void testNullArgsList(){
+        List<String> args = null;
+        ArgumentResolver resolver = new ArgumentResolver();
+        IORedirectionHandler handler = new IORedirectionHandler(args, System.in, System.out, resolver);
+        Throwable thrown = assertThrows(ShellException.class, ()->{handler.extractRedirOptions();});
+        assertEquals("shell: Invalid syntax", thrown.getMessage());
+    }
 
+    @Test
+    void testEmptyArgsList(){
+        List<String> args = Arrays.asList();
+        ArgumentResolver resolver = new ArgumentResolver();
+        IORedirectionHandler handler = new IORedirectionHandler(args, System.in, System.out, resolver);
+        Throwable thrown = assertThrows(ShellException.class, ()->{handler.extractRedirOptions();});
+        assertEquals("shell: Invalid syntax", thrown.getMessage());
+    }
+
+    @Test
+    void testFirstNotRedir(){
+        List<String> args = Arrays.asList("g", "<", TestFileUtils.tempFileName1);
+        ArgumentResolver resolver = new ArgumentResolver();
+        IORedirectionHandler handler = new IORedirectionHandler(args, System.in, System.out, resolver);
+        assertDoesNotThrow(()->{
+            handler.extractRedirOptions();
+            assertEquals(Arrays.asList("g"), handler.getNoRedirArgsList());
+//            OutputStream outstream = new FileOutputStream(TestFileUtils.tempFileName1);
+//            outstream.write(testString.getBytes());
+//            byte[] returnString = new byte[testString.getBytes().length];
+//            handler.getInputStream().read(returnString);
+//            assertEquals(testString.getBytes(), returnString);
+//            assertEquals(System.out, handler.getOutputStream());
+        });
+
+    }
 }
