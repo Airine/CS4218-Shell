8dd194c334ebe99987c19b92497585e7d0488cda
hya <1396592457@qq.com>
2020-02-29 22:45:46 +0800

    add test for diff

--- test/sg/edu/nus/comp/cs4218/impl/app/DiffApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/DiffApplicationTest.java
@@ -8,8 +8,7 @@ import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 import sg.edu.nus.comp.cs4218.impl.util.StringUtils;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,12 +16,18 @@ public class DiffApplicationTest {
 
     DiffInterface diffInterface;
 
+    private static String testText1 = "are you ok?";
+    private static String testText2 = "are you ook?";
+
     @BeforeAll
     static void startUp() {
         try {
             TestFileUtils.createSomeFiles();
-            writeSomethingToFile(TestFileUtils.tempFileName1, "are you ok?");
-            writeSomethingToFile(TestFileUtils.tempFileName2, "are you ook?");
+            writeSomethingToFile(TestFileUtils.tempFileName1, testText1);
+            writeSomethingToFile(TestFileUtils.tempFileName2, testText2);
+
+            writeSomethingToFile(TestFileUtils.tempFileInFolder, testText1);
+            writeSomethingToFile(TestFileUtils.tempFileInBackFolder, testText1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -78,13 +83,108 @@ public class DiffApplicationTest {
                     false, false, false));
         } catch (IOException e) {
             e.printStackTrace();
+            fail("should not throw exception");
         }
     }
 
-//    @Test
-//    void testDiffFolder(){
-//        assertDoesNotThrow(()->);
-//    }
+    @Test
+    void testDiffFolder() {
+        final String[] diff = new String[1];
+        assertDoesNotThrow(() -> diff[0] = diffInterface.diffTwoDir(TestFileUtils.emptyFolderName, TestFileUtils.tempFolderName,
+                false, false, false));
+        assertFalse(StringUtils.isBlank(diff[0]));
+    }
 
+    @Test
+    void testIdentityFolder() {
+        final String[] diff = new String[1];
+        assertDoesNotThrow(() -> diff[0] = diffInterface.diffTwoDir(TestFileUtils.tempFolderName, TestFileUtils.tempBackFolderName,
+                false, false, false));
+        assertTrue(StringUtils.isBlank(diff[0]));
+    }
+
+    @Test
+    void testIdentiyFileAndInputStream() {
+        final String[] diff = new String[1];
+        try (NewIOStream ioStream = new NewIOStream(TestFileUtils.tempFileName2, TestFileUtils.tempFileName2)) {
+            InputStream inputStream = new ByteArrayInputStream(testText1.getBytes());
+            assertDoesNotThrow(() -> diff[0] = diffInterface.diffFileAndStdin(TestFileUtils.tempFileName1, inputStream,
+                    false, false, false));
+            assertTrue(StringUtils.isBlank(diff[0]));
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("should not throw exception");
+        }
+    }
+
+    @Test
+    void testDiffFileAndInputStream() {
+        final String[] diff = new String[1];
+        InputStream inputStream = new ByteArrayInputStream(testText2.getBytes());
+        assertDoesNotThrow(() -> diff[0] = diffInterface.diffFileAndStdin(TestFileUtils.tempFileName1, inputStream,
+                false, false, false));
+        assertFalse(StringUtils.isBlank(diff[0]));
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testRunWithFile() {
+        String[] args = {TestFileUtils.tempFileName1, TestFileUtils.tempFileName1};
+        OutputStream outputStream = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> diffInterface.run(args, System.in, outputStream));
+        assertTrue(StringUtils.isBlank(outputStream.toString()));
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void testRunWithFileAndFlag() {
+        String[] args = {"-s", TestFileUtils.tempFileName1, TestFileUtils.tempFileName1};
+        OutputStream outputStream = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> diffInterface.run(args, System.in, outputStream));
+        assertFalse(StringUtils.isBlank(outputStream.toString()));
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void testRunWithStdin() {
+        String[] args = {TestFileUtils.tempFileName1, "-"};
+        InputStream inputStream = new ByteArrayInputStream((testText1+ "\n     ").getBytes());
+        OutputStream outputStream = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> diffInterface.run(args, inputStream, outputStream));
+        assertFalse(StringUtils.isBlank(outputStream.toString()));
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testRunWithStdinAndFlag() {
+        String[] args = {"-B", TestFileUtils.tempFileName1, "-"};
+        InputStream inputStream = new ByteArrayInputStream((testText1+ "\n     ").getBytes());
+        OutputStream outputStream = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> diffInterface.run(args, inputStream, outputStream));
+        assertTrue(StringUtils.isBlank(outputStream.toString()));
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- test/sg/edu/nus/comp/cs4218/impl/app/TestFileUtils.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/TestFileUtils.java
@@ -7,13 +7,20 @@ import java.io.File;
 final public class TestFileUtils {
 
     private static String currTestDir = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath("test", "temp"));
-    public static String tempFileName1 = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "test1.txt"));
-    public static String tempFileName2 = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "test2.txt"));
-    public static String tempFolderName = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "test-folder"));
-    public static String tempFileInFolder = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "test-folder/test.cc"));
-    public static String emptyFolderName = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "emptyFolder"));
-    public static String notExistFile = FileSystemUtils.getAbsolutePathName(FileSystemUtils.joinPath(currTestDir, "not-exist"));
-    private TestFileUtils(){}
+    public static String tempFileName1 = FileSystemUtils.joinPath(currTestDir, "test1.txt");
+    public static String tempFileName2 = FileSystemUtils.joinPath(currTestDir, "test2.txt");
+
+    public static String tempFolderName = FileSystemUtils.joinPath(currTestDir, "test-folder");
+    public static String tempFileInFolder = FileSystemUtils.joinPath(currTestDir, "test-folder/test.cc");
+
+    public static String tempBackFolderName = FileSystemUtils.joinPath(currTestDir, "test-folder2");
+    public static String tempFileInBackFolder = FileSystemUtils.joinPath(currTestDir, "test-folder2/test2.cc");
+
+    public static String emptyFolderName = FileSystemUtils.joinPath(currTestDir, "emptyFolder");
+    public static String notExistFile = FileSystemUtils.joinPath(currTestDir, "not-exist");
+
+    private TestFileUtils() {
+    }
 
     public static void createSomeFiles() throws Exception {
         File testFolder = new File(FileSystemUtils.getAbsolutePathName(currTestDir));
@@ -32,6 +39,12 @@ final public class TestFileUtils {
             folder.mkdirs();
         }
         FileSystemUtils.createFile(tempFileInFolder);
+
+        File folder2 = new File(tempBackFolderName);
+        if (!folder2.exists()) {
+            folder2.mkdirs();
+        }
+        FileSystemUtils.createFile(tempFileInBackFolder);
     }
 
 
